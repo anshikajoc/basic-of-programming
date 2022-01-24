a99bb7f0e6892003a8ed6bb871c1b478a01a6d52
anshikajoc <98302443+anshikajoc@users.noreply.github.com>
2022-01-24 14:19:31 +0530

    Add files via upload

---
+++ temp conversion.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    //converting temp.
+    float f, c;
+    cout << "enter a temp." << endl;
+    cin >> c;
+    f = (c * 9 / 5) + 32;
+    cout << "tem in ferenhit is :" << f << endl;
+}
